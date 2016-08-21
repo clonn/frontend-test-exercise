54f4df8c441037a04308bfec76d0044f1c0d2eb0
clonn <clonncd@gmail.com>
2016-08-22 06:43:10 +0800

    update for concat

--- mocha-basic/test/add.spec.js
+++ mocha-basic/test/add.spec.js
@@ -4,5 +4,10 @@ describe('Test add function', function() {
     it('test simple add basic', function () {
         var result = add(2, 1);
         result.should.be.equal(3);
+    });
+
+    it('add with a string', function () {
+        var result = add(2, 'kerker');
+        result.should.be.equal(2);
     })
 });
---
+++ mocha-basic/test/concat.spec.js
@@ -0,0 +1,13 @@
+var expect = chai.expect;
+var should = chai.should();
+describe('Test concat function', function() {
+    it('test simple concat basic', function () {
+        var result = concat('hello', 'world');
+        result.should.be.equal('helloworld');
+    });
+
+    it('concat with a number', function () {
+        var result = concat(2, 30);
+        result.should.be.equal('230');
+    })
+});
