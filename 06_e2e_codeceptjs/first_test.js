a85a6c98ecce60e595f45357fb0453c12808c53e
clonn <clonncd@gmail.com>
2016-08-24 08:46:21 +0800

    update for github part

--- 06_e2e_codeceptjs/first_test.js
+++ 06_e2e_codeceptjs/first_test.js
@@ -5,4 +5,5 @@ Scenario('test something', (I) => {
 	// Step1 將方法進行註解起來，可以到 google 網站進行測試
 	// I.amOnPage('https://www.google.com.tw/');
 	// I.see('Google');
+	I.wait(5); 
 });
--- 06_e2e_codeceptjs/github_test.js
+++ 06_e2e_codeceptjs/github_test.js
@@ -1,28 +1,45 @@
+let time = 5;
+
 Scenario('search', (I) => {
   I.amOnPage('https://github.com/search');
+  I.wait(time);
   I.fillField('Search GitHub', 'mokayo');
+  I.wait(time);
   I.pressKey('Enter');
+  I.wait(time);
   I.see('miiixr/mokayo', 'a');
 });
 
-Scenario('signin', (I) => {
-  I.click('Sign in');
-  I.see('Sign in to GitHub');
-  I.fillField('Username or email address', 'user@user.com');
-  I.fillField('Password', '123456');
-  I.click('Sign in');
-  I.see('Incorrect username or password.', '.flash-error');
-});
+// step2 進行首頁按下註冊流程
+// Scenario('signin', (I) => {
+//   I.amOnPage('https://github.com/');
+//   I.click('Sign in');
+//   I.see('Sign in to GitHub');
+//   I.wait(time);
+//   I.fillField('Username or email address', 'user@user.com');
+//   I.wait(time);
+//   I.fillField('Password', '123456');
+//   I.wait(time);
+//   I.click('Sign in');
+//   I.wait(time);
+//   I.see('Incorrect username or password.', '.flash-error');
+// });
 
-Scenario('register', (I) => {
-  within('.js-signup-form', function () {
-    I.fillField('user[login]', 'User');
-    I.fillField('user[email]', 'user@user.com');
-    I.fillField('user[password]', 'user@user.com');
-    I.fillField('q', 'aaa');
-    I.click('button');
-  });
-  I.see('There were problems creating your account.');
-  I.click('Explore');
-  I.seeInCurrentUrl('/explore');
-});
+// step3 進行首頁直接註冊流程
+// Scenario('register', (I) => {
+//   I.amOnPage('https://github.com/');
+//   within('.js-signup-form', function () {
+//     I.fillField('user[login]', 'User');
+//     I.wait(time);
+//     I.fillField('user[email]', 'user@user.com');
+//     I.wait(time);
+//     I.fillField('user[password]', 'user@user.com');
+//     I.wait(time);
+//     I.fillField('q', 'aaa');
+//     I.click('button');
+//   });
+//   I.see('There were problems creating your account.');
+//   I.wait(time);
+//   I.click('Explore');
+//   I.seeInCurrentUrl('/explore');
+// });
